52d1bb2ef3c5a31f5f1eb38c7c467d9b3ab4b438
benSaun <saundebe@mail.gvsu.edu>
2024-11-21 15:05:08 -0500

    Product Store started

--- src/stores/ProductStore.ts
+++ src/stores/ProductStore.ts
@@ -3,5 +3,26 @@ import { ProductDoc } from "../types/product";
 import { initProducts } from "../data-init";
 
 export const useProductStore = defineStore("ProductStore", {
-  // your answer
+  //initializing arrays for all and filtered products
+  state: () => ({
+    products: [] as ProductDoc[],
+    filteredProducts: [] as ProductDoc[],
+  }),
+
+  //creating actions
+  actions: {
+
+    init() {
+      this.products = initProducts;
+      this.filteredProducts = initProducts;
+    },
+    //can filter via category
+    filterByCategory(category:string) {
+      return this.products.filter(product => product.data.category === category);
+    },
+    //can filter via rating above minimum rating
+    filterByRating(minRating:number) {
+      return this.products.filter(product => product.data.rating >= minRating)
+    },
+  },
 });
